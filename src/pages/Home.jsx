bf1c134b7869adb39051b5ce247e0ff442b8c45f
danutavram <avramnicolaedan@gmail.com>
2025-09-05 18:36:24 +0300

    Navbar working progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
+import { StarBackground } from "@/components/StarBackground";
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { Navbar } from "../components/Navbar";
+
 export const Home = ()=>{
-    return <div className="min-h-screen ">
+    return <div className="min-h-screen  bg-background text-foreground overflow-x-hidden">
         
         {/* Theme Toggle */}
-
+        <ThemeToggle />
         {/* Background Effects */}
-
+        <StarBackground />
 
         {/* Navbar */}
-
+        <Navbar />
         {/* Main Content */}
 
         {/* Footer */}
