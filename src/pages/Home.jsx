6638cdbb7f5d93d9a92c5f37af2902941f0fa9f3
danutavram <avramnicolaedan@gmail.com>
2025-09-07 10:07:28 +0300

    AboutSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { StarBackground } from "@/components/StarBackground";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
 
 export const Home = ()=>{
     return <div className="min-h-screen  bg-background text-foreground overflow-x-hidden">
@@ -16,6 +17,7 @@ export const Home = ()=>{
         {/* Main Content */}
         <main>
             <HeroSection />
+            <AboutSection />
         </main>
         {/* Footer */}
     </div>;
