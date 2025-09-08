c822a9f241f2ce10bacb4cd5e5928e53caac0b7b
danutavram <avramnicolaedan@gmail.com>
2025-09-08 12:45:29 +0300

    SkillsSection completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import { StarBackground } from "@/components/StarBackground";
 import { ThemeToggle } from "@/components/ThemeToggle";
-import { Navbar } from "../components/Navbar";
-import { HeroSection } from "../components/HeroSection";
-import { AboutSection } from "../components/AboutSection";
+import { Navbar } from "@/components/Navbar";
+import { HeroSection } from "@/components/HeroSection";
+import { AboutSection } from "@/components/AboutSection";
+import { SkillsSection } from "@/components/SkillsSection";
 
 export const Home = ()=>{
     return <div className="min-h-screen  bg-background text-foreground overflow-x-hidden">
@@ -18,6 +19,7 @@ export const Home = ()=>{
         <main>
             <HeroSection />
             <AboutSection />
+            <SkillsSection />
         </main>
         {/* Footer */}
     </div>;
